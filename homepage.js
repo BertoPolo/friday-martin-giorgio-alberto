4bc480023c96250e7d90e4d65b05b9a9597bdd4f
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-28 15:12:46 +0100

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -20,6 +20,34 @@ const fetchOnLoad = function () {
 
     .catch((err) => console.error(err));
   }
+
+  const fetchOnLoad2 = function () {
+    fetch(
+      "https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem",
+      {}
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        songs2(data);
+  
+      })
+  
+      .catch((err) => console.error(err));
+    }
+
+    const fetchOnLoad3 = function () {
+      fetch(
+        "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen",
+        {}
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          songs3(data);
+    
+        })
+    
+        .catch((err) => console.error(err));
+      }
   
   
   /*FETCH FUNCTIONS END  */
@@ -32,7 +60,33 @@ const fetchOnLoad = function () {
       const song = obj.data;
       console.log(song);
   
-      const eminemContainer = document.querySelector(".home-container");
+      const madonnaContainer = document.querySelector(".home-container");
+      for (let i = 0; i < 8; i++) {
+        let newAnchor = document.createElement("a");
+        
+        newAnchor.innerHTML = `
+            <div class="card-container position-relative d-flex justify-content-center">
+              <img src="assets/playhoverbtn.png" class="playhover2" alt="" />
+              <div>
+              <img class="mt-3 img-card" src="${song[i].album.cover}" alt="Card image cap" />
+              <div class="d-flex flex-column align-items-start p-0 mt-3 ml-1 justify-content-start">
+              <div class="card-title line-clamp2">${song[i].album.title}</div>
+              <p class="card-text mt-n2 line-clamp">${song[i].artist.name}</p>
+              </div>
+              </div>
+            </div>`;
+  
+        newAnchor.onclick = () =>
+          window.location.assign("./AlbumPage.html?albumID=" + song[i].album.id);
+        madonnaContainer.appendChild(newAnchor);
+      }
+    };
+
+    const songs2 = (obj) => {
+      const song = obj.data;
+      console.log(song);
+  
+      const eminemContainer = document.querySelector(".home-container2");
       for (let i = 0; i < 8; i++) {
         let newAnchor = document.createElement("a");
         
@@ -53,6 +107,32 @@ const fetchOnLoad = function () {
         eminemContainer.appendChild(newAnchor);
       }
     };
+
+    const songs3 = (obj) => {
+      const song = obj.data;
+      console.log(song);
+  
+      const queenContainer = document.querySelector(".home-container3");
+      for (let i = 0; i < 8; i++) {
+        let newAnchor = document.createElement("a");
+        
+        newAnchor.innerHTML = `
+            <div class="card-container position-relative d-flex justify-content-center">
+              <img src="assets/playhoverbtn.png" class="playhover2" alt="" />
+              <div>
+              <img class="mt-3 img-card" src="${song[i].album.cover}" alt="Card image cap" />
+              <div class="d-flex flex-column align-items-start p-0 mt-3 ml-1 justify-content-start">
+              <div class="card-title line-clamp2">${song[i].album.title}</div>
+              <p class="card-text mt-n2 line-clamp">${song[i].artist.name}</p>
+              </div>
+              </div>
+            </div>`;
+  
+        newAnchor.onclick = () =>
+          window.location.assign("./AlbumPage.html?albumID=" + song[i].album.id);
+        queenContainer.appendChild(newAnchor);
+      }
+    };
   
   
   
@@ -66,7 +146,8 @@ const fetchOnLoad = function () {
   
   window.onload = () => {
     fetchOnLoad();
-  
+    fetchOnLoad2();
+    fetchOnLoad3();
 
  }
 
