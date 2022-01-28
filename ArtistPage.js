5a65e3a94956483b0fd6800490c89bc9ecad55d0
BertoPolo <bertopolo91@gmail.com>
2022-01-28 13:28:38 +0100

    Update ArtistPage.js

--- ArtistPage.js
+++ ArtistPage.js
@@ -50,6 +50,8 @@ artistNode.addEventListener("click",function(){
 //monthly listeners "nb_fan"   id="listeners"
 //background artist,,, 
 //artist pick img  id . artistPickImg
+//songs
+
 
 const fillingData = function(data){
   const artistNode = document.getElementById("artistName")
@@ -60,8 +62,45 @@ const fillingData = function(data){
   listenersNode.innerText= data.nb_fan
   artistContainerNode.style.backgroundImage = `url(${data.picture_big})`
   artistPickImgNode.src= data.picture_small
+  const songNode = document.getElementsByClassName("song")
+  songNode.innerHTML = ` 
+
+  <div class="row song hiddenSong">
+  <div class="col-md-6 d-flex align-items-center">
+    <span class="listNumber text-muted">10</span>
+    <img
+      src="${data.picture_small}"
+      alt=""
+      class="ml-2"
+    />
+    <p class="pl-3 m-0 text-white">
+      ${data.}<br />
+    </p>
+  </div>
+  <div
+    class="col-md-3 d-flex justify-content-center align-items-center"
+  >
+    <div>
+      <span class="text-muted text-right">705,225,721</span>
+    </div>
+  </div>
+  <div class="col-md-3 d-flex align-items-center">
+    <i class="bi bi-heart hidden heartSong pr-1"></i>
+    <span class="text-muted time px-3">2:02</span>
+    <i class="bi bi-three-dots hidden threeDotsSong text-muted"></i>
+  </div>
+  </div>
+  `
+
+
+
+
 }
 
+
+/* const artistIntroducer = function(){
+
+} */
 
 const goTo = (artistID) =>{
 
