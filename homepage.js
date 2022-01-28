0e99103c0b0a55f4433c78266016324dbb521c2d
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-28 15:00:50 +0100

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -15,61 +15,65 @@ const fetchOnLoad = function () {
     .then((res) => res.json())
     .then((data) => {
       songs(data);
-     
+
     })
+
     .catch((err) => console.error(err));
-}
-
-
-/*FETCH FUNCTIONS END  */
-
-
-/* LOADING FUNCTIONS */
-
-
-  const songs = (obj) => {
-    const song = obj.data;
-    console.log(song);
-
-    const eminemContainer = document.querySelector(".home-container");
-    for (let i = 0; i < song.length; i++) {
-      let newAnchor = document.createElement("a");
-      newAnchor.classList.add("col");
-      newAnchor.innerHTML = `
-          <div class="card-container position-relative d-flex justify-content-center">
-            <img src="assets/playhoverbtn.png" class="playhover2" alt="" />
-            <div>
-            <img class="mt-3 img-card" src="${song[i].album.cover}" alt="Card image cap" />
-            <div class="d-flex flex-column align-items-start p-0 mt-3 ml-1 justify-content-start">
-            <div class="card-title line-clamp2">${song[i].album.title}</div>
-            <p class="card-text mt-n2 line-clamp">${song[i].artist.name}</p>
-            </div>
-            </div>
-          </div>`;
-
-      newAnchor.onclick = () =>
-        window.location.assign("./AlbumPage.html?albumID=" + song[i].album.id);
-      eminemContainer.appendChild(newAnchor);
-    }
-  };
-
-
-
-
-
-/* LOADING FUNCTIONS END */
-
-
-
-/* ONLOAD  FUNCTIONS */
-
-window.onload = () => {
-  fetchOnLoad();
-=======
-      }
-
-     
-     
-     
+  }
+  
+  
+  /*FETCH FUNCTIONS END  */
+  
+  
+  /* LOADING FUNCTIONS */
+  
+  
+    const songs = (obj) => {
+      const song = obj.data;
+      console.log(song);
+  
+      const eminemContainer = document.querySelector(".home-container");
+      for (let i = 0; i < 8; i++) {
+        let newAnchor = document.createElement("a");
         
-         
+        newAnchor.innerHTML = `
+            <div class="card-container position-relative d-flex justify-content-center">
+              <img src="assets/playhoverbtn.png" class="playhover2" alt="" />
+              <div>
+              <img class="mt-3 img-card" src="${song[i].album.cover}" alt="Card image cap" />
+              <div class="d-flex flex-column align-items-start p-0 mt-3 ml-1 justify-content-start">
+              <div class="card-title line-clamp2">${song[i].album.title}</div>
+              <p class="card-text mt-n2 line-clamp">${song[i].artist.name}</p>
+              </div>
+              </div>
+            </div>`;
+  
+        newAnchor.onclick = () =>
+          window.location.assign("./AlbumPage.html?albumID=" + song[i].album.id);
+        eminemContainer.appendChild(newAnchor);
+      }
+    };
+  
+  
+  
+  
+  
+  /* LOADING FUNCTIONS END */
+  
+  
+  
+  /* ONLOAD  FUNCTIONS */
+  
+  window.onload = () => {
+    fetchOnLoad();
+  
+
+ }
+
+
+
+
+
+
+
+
